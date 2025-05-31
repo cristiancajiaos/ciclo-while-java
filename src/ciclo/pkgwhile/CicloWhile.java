bc4628531136b73eff9e938122a7324eb3c6bf6d
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 13:48:22 -0400

    Creado ciclo while

--- src/ciclo/pkgwhile/CicloWhile.java
+++ src/ciclo/pkgwhile/CicloWhile.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ciclo.pkgwhile;
 
-/**
- *
- * @author crist
- */
 public class CicloWhile {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Ciclo While
+        int x = 1;
+        
+         while (x <= 5) {
+             // Instrucciones
+             System.out.println("x: " + x);
+             x++;
+         }
     }
-    
+
 }
